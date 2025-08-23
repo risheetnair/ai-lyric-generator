564f84d689068209e8881c0741caf8d6453f14b8
Risheet Nair <risheetnair@outlook.com>
2025-08-23 18:15:40 -0400

    Initial Spring Boot setup

---
+++ backend/src/main/java/com/risheetnair/ai_lyric_generator/AiLyricGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.risheetnair.ai_lyric_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLyricGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLyricGeneratorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/risheetnair/ai_lyric_generator/AiLyricGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risheetnair.ai_lyric_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLyricGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
